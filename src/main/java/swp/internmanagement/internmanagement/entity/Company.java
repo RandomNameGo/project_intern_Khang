e51d8a9ce0ed70f6c13cffaf7f90cf3575599cce
Admin <khangdoan20122004@gmail.com>
2024-06-03 14:24:08 +0700

    coordinator search and pagination request

--- src/main/java/swp/internmanagement/internmanagement/entity/Company.java
+++ src/main/java/swp/internmanagement/internmanagement/entity/Company.java
@@ -1,5 +1,6 @@
 package swp.internmanagement.internmanagement.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -46,10 +47,11 @@ public class Company {
     private String location;
 
     @OneToMany(mappedBy = "company")
+    @JsonIgnore
     private List<Job> jobs = new ArrayList<>();
 
     @OneToMany(mappedBy = "company")
-    @JsonManagedReference
+    @JsonIgnore
     private List<UserAccount> userAccounts = new ArrayList<>();
 
 }
