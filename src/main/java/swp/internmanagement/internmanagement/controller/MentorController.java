ffc5fb94a666b9bb2566b777a42f8b0d8e942e06
Admin <khangdoan20122004@gmail.com>
2024-06-25 15:54:10 +0700

    fix cors

--- src/main/java/swp/internmanagement/internmanagement/controller/MentorController.java
+++ src/main/java/swp/internmanagement/internmanagement/controller/MentorController.java
@@ -5,15 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import swp.internmanagement.internmanagement.entity.Task;
 import swp.internmanagement.internmanagement.payload.request.CreateTaskRequest;
@@ -27,6 +19,7 @@ import swp.internmanagement.internmanagement.service.TaskService;
 
 @RestController
 @RequestMapping("/internbridge/mentor")
+@CrossOrigin(origins = "http://localhost:3000" , allowCredentials = "true",allowedHeaders = "*")
 public class MentorController {
 
     @Autowired
